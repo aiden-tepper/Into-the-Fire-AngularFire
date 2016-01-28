f93ae48e2b2f6210c7cc002a43c6f8c4ba5004ad
Aiden Tepper <aidenjtep@gmail.com>
2016-01-28 15:06:08 -0800

    Basic angularfire integration

--- js/feedback.js
+++ js/feedback.js
@@ -1,12 +1,90 @@
-var ref = new Firebase(""); //Use your app's Firebase URL
 
-// Add some event handlers for FB events
+var firebase = new Firebase("https://into-the-5ire.firebaseio.com/");
 
-$(document).ready(function(){
-  console.log("Document loaded");
+function update(data){
+  $("#feedback-list").html("<br>");
+  // for(var fbelem in data){
+    var elem = data[fbelem];
+    var feedbackList =
+    "<div id='" + fbelem + "'><h3><i>" + elem.timestamp + ":</i></h3>" +
+    "<h3>" + elem.fullname + " - " + elem.feedback + "</h3>" +
+    "<h3>" + elem.faves + " favorites</h3>" +
+    "<img id='heart' src='img/heart_outlined.png' width='5%'>" + "</div>";
+    $("#feedback-list").append(feedbackList);
+    $("#feedback-list").append("<br>");
+    $("#" + fbelem).data("id", fbelem);
+    $("#" + fbelem).data("faves", elem.faves);
+  }
+  addEventListeners();
+}
+
+function addEventListeners() {
+
+  $(document).on({
+    mouseenter: function() {
+      $(this).attr("src", "img/heart_filled.png");
+      console.log("filled");
+    },
+    mouseleave: function() {
+      $(this).attr("src", "img/heart_outlined.png");
+      console.log("outlined");
+    }
+  }, "#heart");
+
+  // $(document).on({
+    click: function(){
+      var key = $(this).parent().data("id");
+      var faves = $(this).parent().data("faves");
+      faves ++;
+      firebase.child(key).update({
+        "faves": faves
+      });
+    }
+  }, "#heart");
+
+  $("#submit_button").click(function(){
+    $("#addModal").foundation("reveal", "close");
+  });
+
+}
+
+
+function updateDOM() {
+
+  // firebase.on("value", function(snapshot){
+    var data = snapshot.val();
+    console.log("Snapshot recieved:");
+    console.log(data);
+    update(data);
+    console.log("Snapshot loaded into DOM");
+    if(snapshot.val() == null) {
+      $("#feedback-list").html("<br><h3>Be the first to add feedback!<h3>");
+    }
+  }, function(errorObject){
+    console.log("The read failed: " + errorObject.code);
+  });
+
   $("#add-form").submit(function(event){
     event.preventDefault();
-    console.log("Form submitted!");
-    // Go grab your info and do something with it.
+    console.log("Form submitted");
+    var fullname = $("#name").val();
+    var feedback = $("#feedback").val();
+    var date = new Date();
+    var timestamp = date.toLocaleDateString() + " at " + date.toLocaleTimeString();
+    var JSONObj = {
+      "fullname":fullname,
+      "feedback":feedback,
+      "timestamp":timestamp,
+      "faves": 0
+    };
+    firebase.push(JSONObj, function(){
+      console.log("Push to Firebase was successful");
+    });
   });
+
+}
+
+$(document).ready(function(){
+  updateDOM();
+  console.log("Document loaded");
 });
