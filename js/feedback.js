9024b4be003d004e87c28677bf8f60736e6dd689
Aiden Tepper <aidenjtep@gmail.com>
2016-02-01 15:09:42 -0800

    Worked a little

--- js/feedback.js
+++ js/feedback.js
@@ -1,43 +1,34 @@
-var app = angular.module("app", ["firebase"]);
+angular.module("app", ["firebase"])
+.controller("controller", ["$scope", "$firebaseObject", function($scope, $firebaseObject) {
 
-app.controller("controller", ["$scope", "$firebaseArray",
-  function($scope, $firebaseArray) {
+  var firebase = new Firebase("https://into-the-5ire.firebaseio.com/");
 
-
-
-  
-    var firebase = new Firebase("https://into-the-5ire.firebaseio.com/");
-
-      $("#add-form").submit(function(event){
-        event.preventDefault();
-        console.log("Form submitted");
-        var fullname = $("#name").val();
-        var feedback = $("#feedback").val();
-        var date = new Date();
-        var timestamp = date.toLocaleDateString() + " at " + date.toLocaleTimeString();
-        var JSONObj = {
-          "fullname":fullname,
-          "feedback":feedback,
-          "timestamp":timestamp,
-          "faves": 0
-        };
-        firebase.push(JSONObj, function(){
-          console.log("Push to Firebase was successful");
-        });
-      });
-
-    }
-
-    $(document).ready(function(){
-      updateDOM();
-      console.log("Document loaded");
+  $("#add-form").submit(function(event){
+    event.preventDefault();
+    console.log("Form submitted");
+    var fullname = $("#name").val();
+    var feedback = $("#feedback").val();
+    var date = new Date();
+    var timestamp = date.toLocaleDateString() + " at " + date.toLocaleTimeString();
+    var JSONObj = {
+      "fullname":fullname,
+      "feedback":feedback,
+      "timestamp":timestamp,
+      "faves": 0
+    };
+    firebase.push(JSONObj, function(){
+      console.log("Push to Firebase was successful");
     });
+  });
+
+}
+
+$(document).ready(function(){
+  updateDOM();
+  console.log("Document loaded");
+});
 
 
 
 
-
-
-
-  }
-]);
+}]);
